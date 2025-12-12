34eb9a603c6abbe016ed2311a718d5f1d1a15d16
Alex <alexander.barabash@userway.org>
2025-12-12 09:08:25 +0200

    switch server

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -3,5 +3,6 @@ export default {
  organization: "ivan-haliurov-levelaccess-com-hspeq",
  project: "level-ci-sample-github-1",
  token: process.env.LEVEL_CI_TOKEN,
- reportPaths: ['./level-ci-reports']
+ reportPaths: ['./level-ci-reports'],
+ server: "https://api.dev.userway.dev"
 } satisfies Config;
